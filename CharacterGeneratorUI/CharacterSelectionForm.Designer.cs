75c0dfefef6e14911399955cabd389a1f505f7fe
Caarnus <lewellym4243@gmail.com>
2017-11-09 21:20:07 -0600

    Initial UI design for main menu

--- CharacterGeneratorUI/CharacterSelectionForm.Designer.cs
+++ CharacterGeneratorUI/CharacterSelectionForm.Designer.cs
@@ -28,12 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CharacterSelectionForm));
+            this.newChar_button = new System.Windows.Forms.Button();
+            this.loadChar_button = new System.Windows.Forms.Button();
+            this.createRace_button = new System.Windows.Forms.Button();
+            this.createSubrace_button = new System.Windows.Forms.Button();
+            this.createClass_button = new System.Windows.Forms.Button();
+            this.createArch_button = new System.Windows.Forms.Button();
+            this.createBack_button = new System.Windows.Forms.Button();
+            this.createEquip_button = new System.Windows.Forms.Button();
+            this.createSpell_button = new System.Windows.Forms.Button();
+            this.title_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // newChar_button
+            // 
+            this.newChar_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newChar_button.Location = new System.Drawing.Point(212, 102);
+            this.newChar_button.Name = "newChar_button";
+            this.newChar_button.Size = new System.Drawing.Size(185, 33);
+            this.newChar_button.TabIndex = 0;
+            this.newChar_button.Text = "New Character";
+            this.newChar_button.UseVisualStyleBackColor = true;
+            // 
+            // loadChar_button
+            // 
+            this.loadChar_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadChar_button.Location = new System.Drawing.Point(403, 102);
+            this.loadChar_button.Name = "loadChar_button";
+            this.loadChar_button.Size = new System.Drawing.Size(185, 33);
+            this.loadChar_button.TabIndex = 1;
+            this.loadChar_button.Text = "Load Character";
+            this.loadChar_button.UseVisualStyleBackColor = true;
+            // 
+            // createRace_button
+            // 
+            this.createRace_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createRace_button.Location = new System.Drawing.Point(12, 513);
+            this.createRace_button.Name = "createRace_button";
+            this.createRace_button.Size = new System.Drawing.Size(185, 33);
+            this.createRace_button.TabIndex = 2;
+            this.createRace_button.Text = "Create Race";
+            this.createRace_button.UseVisualStyleBackColor = true;
+            // 
+            // createSubrace_button
+            // 
+            this.createSubrace_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createSubrace_button.Location = new System.Drawing.Point(12, 552);
+            this.createSubrace_button.Name = "createSubrace_button";
+            this.createSubrace_button.Size = new System.Drawing.Size(185, 33);
+            this.createSubrace_button.TabIndex = 3;
+            this.createSubrace_button.Text = "Create Subrace";
+            this.createSubrace_button.UseVisualStyleBackColor = true;
+            // 
+            // createClass_button
+            // 
+            this.createClass_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createClass_button.Location = new System.Drawing.Point(587, 513);
+            this.createClass_button.Name = "createClass_button";
+            this.createClass_button.Size = new System.Drawing.Size(185, 33);
+            this.createClass_button.TabIndex = 4;
+            this.createClass_button.Text = "Create Class";
+            this.createClass_button.UseVisualStyleBackColor = true;
+            // 
+            // createArch_button
+            // 
+            this.createArch_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createArch_button.Location = new System.Drawing.Point(587, 552);
+            this.createArch_button.Name = "createArch_button";
+            this.createArch_button.Size = new System.Drawing.Size(185, 33);
+            this.createArch_button.TabIndex = 5;
+            this.createArch_button.Text = "Create Archetype";
+            this.createArch_button.UseVisualStyleBackColor = true;
+            // 
+            // createBack_button
+            // 
+            this.createBack_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createBack_button.Location = new System.Drawing.Point(116, 852);
+            this.createBack_button.Name = "createBack_button";
+            this.createBack_button.Size = new System.Drawing.Size(185, 33);
+            this.createBack_button.TabIndex = 6;
+            this.createBack_button.Text = "Create Background";
+            this.createBack_button.UseVisualStyleBackColor = true;
+            // 
+            // createEquip_button
+            // 
+            this.createEquip_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createEquip_button.Location = new System.Drawing.Point(307, 852);
+            this.createEquip_button.Name = "createEquip_button";
+            this.createEquip_button.Size = new System.Drawing.Size(185, 33);
+            this.createEquip_button.TabIndex = 7;
+            this.createEquip_button.Text = "Create Equipment";
+            this.createEquip_button.UseVisualStyleBackColor = true;
+            // 
+            // createSpell_button
+            // 
+            this.createSpell_button.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createSpell_button.Location = new System.Drawing.Point(498, 852);
+            this.createSpell_button.Name = "createSpell_button";
+            this.createSpell_button.Size = new System.Drawing.Size(185, 33);
+            this.createSpell_button.TabIndex = 8;
+            this.createSpell_button.Text = "Create Spell";
+            this.createSpell_button.UseVisualStyleBackColor = true;
+            // 
+            // title_label
+            // 
+            this.title_label.AutoSize = true;
+            this.title_label.Font = new System.Drawing.Font("Monotype Corsiva", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title_label.ForeColor = System.Drawing.Color.Black;
+            this.title_label.Location = new System.Drawing.Point(163, 0);
+            this.title_label.Name = "title_label";
+            this.title_label.Size = new System.Drawing.Size(474, 79);
+            this.title_label.TabIndex = 9;
+            this.title_label.Text = "Character Creation";
+            // 
+            // CharacterSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(784, 961);
+            this.Controls.Add(this.title_label);
+            this.Controls.Add(this.createSpell_button);
+            this.Controls.Add(this.createEquip_button);
+            this.Controls.Add(this.createBack_button);
+            this.Controls.Add(this.createArch_button);
+            this.Controls.Add(this.createClass_button);
+            this.Controls.Add(this.createSubrace_button);
+            this.Controls.Add(this.createRace_button);
+            this.Controls.Add(this.loadChar_button);
+            this.Controls.Add(this.newChar_button);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CharacterSelectionForm";
+            this.Text = "Character Selection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button newChar_button;
+        private System.Windows.Forms.Button loadChar_button;
+        private System.Windows.Forms.Button createRace_button;
+        private System.Windows.Forms.Button createSubrace_button;
+        private System.Windows.Forms.Button createClass_button;
+        private System.Windows.Forms.Button createArch_button;
+        private System.Windows.Forms.Button createBack_button;
+        private System.Windows.Forms.Button createEquip_button;
+        private System.Windows.Forms.Button createSpell_button;
+        private System.Windows.Forms.Label title_label;
     }
 }
 
